d74e3f6827d92f360a65dc868aeddec7cc2eb527
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2019-01-24 12:22:24 +0300

    Updating travis

--- app/views/users.py
+++ app/views/users.py
@@ -13,15 +13,16 @@ userdb.default_users()
 
 users_view = Blueprint('users_view', __name__)
 
+
 @users_view.route('/ireporter/api/v2/auth/signup', methods=["POST"])
 @swag_from('../docs/users/signup.yml')
-def postuser():
+def postuser(): 
 
     """ function to add a user """
-    try:
+    try: 
         data = request.get_json()
-    except:
-        return jsonify({"status":400, "error":"No data posted"}), 400
+    except Exception: 
+        return jsonify({"status": 400, "error": "No data posted"}), 400
 
     new_user = User(**data)
     new_user.id = generate_id()
@@ -33,84 +34,84 @@ def postuser():
     validate_user = Validate_user()
     thisuser = validate_user.validate(**user)
 
-    if not thisuser['message'] == 'successfully validated':
-        return jsonify({"status":400, "error":thisuser['message']}), 400
+    if not thisuser['message'] == 'successfully validated': 
+        return jsonify({"status": 400, "error": thisuser['message']}), 400
 
     result = userdb.register_user(**user)
 
-    if result=='False':
-        return jsonify({"status":400, "error":"User already exists"}), 400
+    if result=='False': 
+        return jsonify({"status": 400, "error": "User already exists"}), 400
 
 
 
-    return jsonify({"status":201,
-                    "data":[{
-                        "user":userdb.check_id(new_user.id),
+    return jsonify({"status": 201,
+                    "data": [{
+                        "user": userdb.check_id(new_user.id),
                     }]}), 201
 
 
 @users_view.route('/ireporter/api/v2/users', methods=["GET"])
 @jwt_required()
 @swag_from('../docs/users/getusers.yml')
-def getusers():
+def getusers(): 
 
     """ function to get all users """
 
-    if not current_identity['is_admin']:
-        return jsonify({"status":401,
-                        "data":[{
-                            "message":"Sorry! Access restricted to administrators.",
+    if not current_identity['is_admin']: 
+        return jsonify({"status": 401,
+                        "data": [{
+                            "message": "Sorry! Access restricted to administrators.",
                         }]}), 401
 
     users = userdb.users()
 
-    return jsonify({"status":200,
-                    "data":users
+    return jsonify({"status": 200,
+                    "data": users
                     }), 200
 
   
-@users_view.route('/ireporter/api/v2/users/<int:id>', methods=["GET"])
+@users_view.route('/ireporter/api/v2/users/<int: id>', methods=["GET"])
 @jwt_required()
 @swag_from('../docs/users/getauser.yml')
-def getauser(id):
+def getauser(id): 
 
     """ function to get a user by id """
 
-    if not (current_identity['is_admin'] or (current_identity['userid'] == id)):
-        return jsonify({"error":"Sorry! Access denied.",
+    if not (current_identity['is_admin'] or (current_identity['userid'] == id)): 
+        return jsonify({"error": "Sorry! Access denied.",
                         }), 401
 
     user = userdb.check_id(id)
 
-    if user and user != 'False':
-        return jsonify({"status":200,
-                        "data":[user]
+    if user and user != 'False': 
+        return jsonify({"status": 200,
+                        "data": [user]
                         }), 200
 
-    return jsonify({"status":404, "error":"User not found"}), 404
+    return jsonify({"status": 404, "error": "User not found"}), 404
 
-@users_view.route('/ireporter/api/v2/users/<int:id>', methods=["PUT"])
+@users_view.route('/ireporter/api/v2/users/<int: id>', methods=["PUT"])
 @jwt_required()
 @swag_from('../docs/users/updateuser.yml')
-def updateuser(id):
+def updateuser(id): 
     """ function to update user data """
 
-    if not (current_identity['is_admin'] or (current_identity['userid'] == id)):
-        return jsonify({"status":401,
-                        "data":[{
-                            "message":"Sorry! Access denied.",
+    if not (current_identity['is_admin'] or (current_identity['userid'] == id)): 
+        return jsonify({"status": 401,
+                        "data": [{
+                            "message": "Sorry! Access denied.",
                         }]}), 401
 
-    try:
+    try: 
         data = request.get_json()
-    except:
-        return jsonify({"status":400, "error":"No data posted"}), 400
+    except Exception: 
+        return jsonify({"status": 400, "error": "No data posted"}), 400
 
     user = userdb.check_id(id)
-    if not user or user == 'False':
-        return jsonify({"status":404, "error":"User not found"}), 404
+    if not user or user == 'False': 
+        return jsonify({"status": 404, "error": "User not found"}), 404
 
-    for key in data:
+    for key in data: 
         user[key] = data[key]
 
     user['id'] = user['userid']
@@ -119,17 +120,17 @@ def updateuser(id):
     validate_user = Validate_user()
     thisuser = validate_user.validate(**user)
 
-    if not thisuser['message'] == 'successfully validated':
-        return jsonify({"status":400, "error":thisuser['message']}), 400
+    if not thisuser['message'] == 'successfully validated': 
+        return jsonify({"status": 400, "error": thisuser['message']}), 400
 
     result = userdb.update(**user)
 
-    if result=='False':
-        return jsonify({"status":400, "error":"User update failed"}), 400
+    if result=='False': 
+        return jsonify({"status": 400, "error": "User update failed"}), 400
 
-    return jsonify({"status":200,
-                    "data":[{
-                        "id":id,
-                        "message":"Updated User record",
+    return jsonify({"status": 200,
+                    "data": [{
+                        "id": id,
+                        "message": "Updated User record",
                     }]}), 200
 
