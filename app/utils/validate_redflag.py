9afc570e74771645930eec4ff708aa0753dee9f7
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2019-02-15 08:15:35 +0300

    Update validate_redflag.py

--- app/utils/validate_redflag.py
+++ app/utils/validate_redflag.py
@@ -1,20 +1,22 @@
 
 from app.utils.validator import Validator
 
+
 class Validate_redflag(Validator):
 
     """ class inherits from validator """
 
     def __init__(self):
-            
         """ Method declares the schemma aganist which a redflag is to be validated """
 
         schema = [
-            {'key':'location', 'type':'string', 'not_null':True, 'max_length':18},
-            {'key':'comment', 'type':'string', 'not_null':True, 'max_length':100},
-            {'key':'createdon', 'type':'string', 'not_null':True},
-            {'key':'createdby', 'type':'integer', 'not_null':True},
-            {'key':'title', 'type':'string', 'not_null':True, 'max_length':18},
-            {'key':'status', 'type':'string', 'not_null':True}
+            {'key': 'location', 'type': 'string',
+                'not_null': True, 'max_length': 40},
+            {'key': 'comment', 'type': 'string',
+                'not_null': True, 'max_length': 500},
+            {'key': 'createdon', 'type': 'string', 'not_null': True},
+            {'key': 'createdby', 'type': 'integer', 'not_null': True},
+            {'key': 'title', 'type': 'string', 'not_null': True, 'max_length': 50},
+            {'key': 'status', 'type': 'string', 'not_null': True}
         ]
         Validator.__init__(self, schema)
