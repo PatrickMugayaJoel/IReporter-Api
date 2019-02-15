8ed1ad0fa657a9c749a1b516a4a8bd6e31a924d1
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2019-02-15 09:39:45 +0300

    Update utils.py

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -6,7 +6,6 @@ from database.incidents_db import IncidentsDB
 
 
 def serialize(objt):
-
     """ Method receives an object and returns a dictionary """
 
     if isinstance(objt, list):
@@ -14,10 +13,11 @@ def serialize(objt):
         for item in objt:
             listtwo.append(serialize(item))
         return listtwo
-    else: return objt.__dict__
+    else:
+        return objt.__dict__
+
 
 def get_flag_by_id(id):
-
     """ get flag by id """
 
     incidents_db = IncidentsDB()
@@ -28,20 +28,3 @@ def get_flag_by_id(id):
     else:
         regflag = None
         return regflag
-
-def encode_handler(identity, JWT_SECRET_KEY, JWT_ALGORITHM):
-
-    """ method generates a jwt token """
-
-    payload = payload_handler(identity)
-    return jwt.encode(payload, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM, headers=None)
-
-def payload_handler(identity):
-
-    """ method builds playload that will be added to the JWT token  """
-
-    iat = datetime.utcnow()
-    exp = iat + timedelta(hours=20)
-    nbf = iat + timedelta(seconds=0)
-    identity = getattr(identity, 'id') or identity['id']
-    return {'exp': exp, 'iat': iat, 'nbf': nbf, 'identity': identity}
